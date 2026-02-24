8400621835a3a64e47eb281c54fd25e4c83a0aba
mehdilamnaouar19-crypto <mehdilamnaouar19@gmail.com>
2026-02-24 20:19:06 +0000

    server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const puppeteer = require("puppeteer");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.post("/check", async (req, res) => {
+  const { plate } = req.body;
+
+  try {
+
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox','--disable-setuid-sandbox']
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto("https://opposition.narsa.gov.ma:82", {
+      waitUntil: "networkidle2"
+    });
+
+    await page.type("#txtImmatriculation", plate);
+
+    await page.click("#btnRechercher");
+
+    await page.waitForTimeout(5000);
+
+    const result = await page.evaluate(() => {
+      return document.body.innerText;
+    });
+
+    await browser.close();
+
+    res.json({ success: true, result });
+
+  } catch (err) {
+    res.json({ success: false, error: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000);
